9351c9ce9acb150dc11a16bae622f289d596fc41
tim <78782311+westleft@users.noreply.github.com>
2022-06-11 15:03:02 +0800

    05-advanced-typing

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const inputs = document.querySelectorAll(".num");
+inputs.forEach(item => {
+    console.log(item.textContent);
+});
+class Calculator {
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const inputs: NodeList = document.querySelectorAll(".num");
+
+inputs.forEach(item=>{
+    console.log(item.textContent);
+    
+})
+
+class Calculator {
+
+}
+
