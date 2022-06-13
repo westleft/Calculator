3a33241a03322b886c894cdf7ef2322df1e6790e
tim <78782311+westleft@users.noreply.github.com>
2022-06-13 23:09:57 +0800

    改TS架構

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,28 @@
 "use strict";
-const inputs = document.querySelectorAll(".num");
-inputs.forEach(item => {
-    console.log(item.textContent);
-});
 class Calculator {
+    constructor() {
+        this.userInput = document.querySelector(".display");
+        this.formula = document.querySelector(".formula");
+        this.inputs = document.querySelectorAll(".input");
+        this.equalButton = document.querySelector(".equal");
+        this.result = "";
+        this.addNumber();
+    }
+    addNumber() {
+        this.inputs.forEach((item) => {
+            item.addEventListener("click", () => {
+                this.result += item.textContent;
+                this.userInput.value = this.result;
+            });
+        });
+        this.equalButton.addEventListener("click", () => {
+            this.formula.value = this.result;
+            const result = this.replaceOperation(this.result);
+            this.userInput.value = eval(result);
+        });
+    }
+    replaceOperation(str) {
+        return str.replace(/÷/g, "/").replace(/×/g, "*");
+    }
 }
+const calculator = new Calculator();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-const inputs: NodeList = document.querySelectorAll(".num");
-
-inputs.forEach(item=>{
-    console.log(item.textContent);
-    
-})
-
 class Calculator {
+  userInput: HTMLInputElement;
+  formula: HTMLInputElement;
+  inputs: NodeList;
+  result: string;
+  equalButton: HTMLButtonElement;
 
+  constructor() {
+    this.userInput = document.querySelector(".display")! as HTMLInputElement;
+    this.formula = document.querySelector(".formula")! as HTMLInputElement;
+    this.inputs = document.querySelectorAll(".input")! as NodeList;
+    this.equalButton = document.querySelector(".equal")! as HTMLButtonElement;
+
+    this.result = "";
+    this.addNumber();
+  }
+
+  private addNumber() {
+    this.inputs.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.result += item.textContent;
+        this.userInput.value = this.result;
+      });
+    });
+
+    this.equalButton.addEventListener("click", () => {
+      this.formula.value = this.result;
+
+      const result = this.replaceOperation(this.result);
+      this.userInput.value = eval(result);
+    });
+  }
+
+  private replaceOperation(str: string) {
+    return str.replace(/÷/g, "/").replace(/×/g, "*");
+  }
 }
 
+const calculator = new Calculator();
